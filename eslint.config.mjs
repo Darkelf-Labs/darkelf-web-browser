e897ce89235e50be4ce597ddd568f0918a474b41
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-07-29 02:50:30 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-export default [
+const config = [
   ...nextVitals,
   {
     languageOptions: {
@@ -14,3 +14,5 @@ export default [
     },
   },
 ];
+
+export default config;
